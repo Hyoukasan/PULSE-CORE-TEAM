52270b2f0366ae6d313c849c727f6772ba1e11ea
HyoukaSan <keynotyn13@mail.ru>
2026-03-04 18:56:28 +0300

    update

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,15 +1,19 @@
+import os
+
 from flask import Flask
 import logging
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from app.main import config
 
-def create_app(config_name = None):
+def create_app(config_name=None):
     app = Flask(__name__)
 
+    load_dotenv(".env")
+
+
     if config_name is None:
-        config_name = "default"
+        config_name = os.environ.get('FLASK_ENV', 'development') # devmode
 
 
     return app
 
-env = dotenv_values(".env")
--- app/main/config.py
+++ app/main/config.py
@@ -1,15 +1,19 @@
 import os
 
 class Config:
-    SECRET_KEY = oc.environ.get('SECRET_KEY') or 'Access denied'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Access denied'
+
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pulse_project.db'
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pulse_project.db'
 
 class TestingConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pulse_test.db'
 
 
 config = {
