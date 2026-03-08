aee7d8fc65237750ebc6372ed8dc085a8437d9db
HyoukaSan <keynotyn13@mail.ru>
2026-03-08 22:29:14 +0300

    update

--- app/src/domain/user.py
+++ app/src/domain/user.py
@@ -1,1 +1,10 @@
 
+from ..integrations.db import db
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Collumn(db.Intager, primary_key=True)
+    username = db.Collumn(db.String(80),nullable=False,enique=True)
+
